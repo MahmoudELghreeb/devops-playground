0482575f90e3c5b15b70e8603e84b93b0100b1cd
MahmoudELghreeb <myprivatemahmoudelghreeb15222@gmail.com>
2025-09-16 14:35:19 -0400

    Initial commit: Add file manager script and project structure

---
+++ scripts/file-manager.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Simple File Manager Script
+echo "=== DevOps Playground File Manager ==="
+echo "1. Create file"
+echo "2. Delete file"
+echo "3. Archive folder"
+echo "4. Exit"
+read -p "Choose option: " option
+
+case $option in
+    1)
+        read -p "Enter filename: " fname
+        touch "$fname"
+        echo "File $fname created."
+        ;;
+    2)
+        read -p "Enter filename to delete: " fname
+        rm -f "$fname"
+        echo "File $fname deleted."
+        ;;
+    3)
+        read -p "Enter folder name to archive: " folder
+        if [ -d "$folder" ]; then
+            tar -czvf "${folder}.tar.gz" "$folder"
+            echo "Folder $folder archived."
+        else
+            echo "Folder not found!"
+        fi
+        ;;
+    4)
+        echo "Goodbye!"
+        exit 0
+        ;;
+    *)
+        echo "Invalid option!"
+        ;;
+esac
